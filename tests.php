0066064d549bea8cc3a9b6e048315f57a9f21344
Git I Hate You <git@screenfeed.fr>
2018-08-31 17:10:09 +0200

    Hotfix: set a filesystem object in tests.php

--- tests.php
+++ tests.php
@@ -1,4 +1,8 @@
 <?php
 defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 
-global $wpdb, $wp_filter;
+global $wpdb, $wp_filter, $wp_rewrite;
+
+if ( function_exists( 'imagify_get_filesystem' ) ) {
+	$filesystem = imagify_get_filesystem();
+}
