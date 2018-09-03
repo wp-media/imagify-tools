8a24cacb27b8555ab72a1d3803bad5100c909338
Git I Hate You <git@screenfeed.fr>
2018-09-03 16:29:06 +0200

    Hotfix: no need to test for imagify_get_filesystem() existance since it's in compat.php

--- tests.php
+++ tests.php
@@ -3,6 +3,4 @@ defined( 'ABSPATH' ) || die( 'Cheatin\' uh?' );
 
 global $wpdb, $wp_filter, $wp_rewrite;
 
-if ( function_exists( 'imagify_get_filesystem' ) ) {
-	$filesystem = imagify_get_filesystem();
-}
+$filesystem = imagify_get_filesystem();
