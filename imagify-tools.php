350fa72a58bb643d6b5dbc9e7ad7bbf03ee4f4f4
Git I Hate You <git@screenfeed.fr>
2018-12-15 00:57:21 +0100

    Hotfix: bump version to 1.0.5.1

--- imagify-tools.php
+++ imagify-tools.php
@@ -3,7 +3,7 @@
  * Plugin Name: Imagify Tools
  * Plugin URI: https://wordpress.org/plugins/imagify/
  * Description: A WordPress plugin helping debug in Imagify.
- * Version: 1.0.5
+ * Version: 1.0.5.1
  * Author: WP Media
  * Author URI: https://wp-media.me/
  * Licence: GPLv2
@@ -35,7 +35,7 @@ function imagify_tools_init() {
 	$plugin_dir .= DIRECTORY_SEPARATOR;
 
 	// Define plugin constants.
-	define( 'IMAGIFY_TOOLS_VERSION',        '1.0.5' );
+	define( 'IMAGIFY_TOOLS_VERSION',        '1.0.5.1' );
 	define( 'IMAGIFY_TOOLS_FILE',           $plugin_file );
 	define( 'IMAGIFY_TOOLS_PATH',           $plugin_dir );
 	define( 'IMAGIFY_TOOLS_CLASSES_PATH',   IMAGIFY_TOOLS_PATH . 'classes' . DIRECTORY_SEPARATOR );
